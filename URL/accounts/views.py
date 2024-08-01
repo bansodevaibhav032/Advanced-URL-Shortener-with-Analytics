c722b83b55b42e262e1351dd43cdf60f5a193aab
bansodevaibhav032 <129043443+bansodevaibhav032@users.noreply.github.com>
2024-08-01 19:01:14 +0530

    Authentication Backend in Django

--- URL/accounts/views.py
+++ URL/accounts/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
 def login(request):
+    if request.method=='POST':
+        username=request.POST.get("username",None)
+        password=request.POST.get("password",None)
+        if username and password:
+            user= auth.authenticate(request,username=username,password=password)
+            if user:
+                auth.login(request,user)
+                return render(request,"home.html")                       
+            return render(request,"login.html",{'data':"Incorrect Credantials"})
     return render(request,"login.html")
 
 def register(request):
+    if request.method=='POST':
+        username=request.POST.get("username",None)
+        password=request.POST.get("password",None)
+        email=request.POST.get("email",None)
+        fname=request.POST.get("fname",None)
+        lname=request.POST.get("lname",None)
+        
+        user=User.objects.create_user(username=username,password=password,email=email)
+        user.save()
+        
+        auth.login(request,user)
+        return render(request,'home.html')
     return render(request,"register.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Admin\OneDrive\Desktop\Coding\PROJECTS\Main Projects\Advanced-URL-Shortener-with-Analytics\myenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
