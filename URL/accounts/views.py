1dfd20bf29086c79491a8a32c66bf44a35f89fda
bansodevaibhav032 <129043443+bansodevaibhav032@users.noreply.github.com>
2024-08-02 14:35:33 +0530

    JavaScript Alert using Bootstrap
    Use of the Django Message and Bootstrap model to create the JavaScript Alert.

--- URL/accounts/views.py
+++ URL/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth,messages
 
 # Create your views here.
 
@@ -12,8 +12,10 @@ def login(request):
             user= auth.authenticate(request,username=username,password=password)
             if user:
                 auth.login(request,user)
-                return render(request,"home.html")                       
-            return render(request,"login.html",{'data':"Incorrect Credantials"})
+                messages.success(request,"Successfully Login")
+                return redirect("/")                       
+            messages.error(request,"Incorrect Credantials")
+            return render(request,"login.html",{"username":username})
     return render(request,"login.html")
 
 def register(request):
@@ -23,12 +25,16 @@ def register(request):
         email=request.POST.get("email",None)
         fname=request.POST.get("fname",None)
         lname=request.POST.get("lname",None)
-        
-        user=User.objects.create_user(username=username,password=password,email=email)
-        user.save()
-        
-        auth.login(request,user)
-        return render(request,'home.html')
+        try:
+
+            user=User.objects.create_user(username=username,password=password,email=email)
+            user.save()
+            auth.login(request,user)
+        except:
+            messages.error(request,"User with this Username, Already Exists !!")
+            return render(request,"register.html",{"username":username,"email":email,"fname":fname,"lname":lname})  
+        messages.success(request,"User Creation Successfully")
+        return redirect('/')
     return render(request,"register.html")
 
 def logout(request):
